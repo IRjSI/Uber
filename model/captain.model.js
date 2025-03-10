23c15a1f5241daef6baf85ba1adf556a8e945c46
IRjSI <rajusuthar0077@gmail.com>
2025-03-10 19:24:46 +0530

    implement captain model

---
+++ model/captain.model.js
@@ -0,0 +1,77 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const captainSchema = new Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        lastname: {
+            type: String,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        socketId: {
+            type: String
+        },
+        // if captain is available or not
+        status: {
+            type: String,
+            enum: ['active', 'inactive'],
+            default: 'inactive'
+        },
+        vehicle: {
+            color: {
+                type: String,
+                required: true
+            },
+            plate: {
+                type: String,
+                required: true
+            },
+            capacity: {
+                type: Number,
+                required: true
+            },
+            vehicleType: {
+                type: String,
+                required: true,
+                enum: ['car', 'bike', 'rickshaw']
+            }
+        },
+        location: {
+            lat: {
+                type: Number
+            },
+            lng: {
+                type: Number
+            }
+        }
+    }
+});
+
+captainSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id:this._id }, process.env.JWT_SECRET, { expiresIn: '24h' })
+}
+
+captainSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+captainSchema.statics.hashPassword = async function(password) {
+    return await bcrypt.hash(password,10);
+}
+
+const captainModel = mongoose.model('captain', captainSchema);
+
+export {captainModel};
